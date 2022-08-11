98d9b943204beb88d6e21e598655e337446ee98d
yisheng425 <1284780636@qq.com>
2022-08-11 02:27:10 -0400

    first time to upload github

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -290,7 +290,7 @@ sys_open(void)
   int fd, omode;
   struct file *f;
   struct inode *ip;
-  int n;
+  int n, depth = 0;
 
   if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
     return -1;
@@ -320,6 +320,25 @@ sys_open(void)
     iunlockput(ip);
     end_op();
     return -1;
+  }
+
+  while(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+    if (depth++ >= 20) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    if(readi(ip, 0, (uint64)path, 0, MAXPATH) < MAXPATH) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    iunlockput(ip);
+    if((ip = namei(path)) == 0) {
+      end_op();
+      return -1;
+    }
+    ilock(ip);
   }
 
   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
@@ -482,5 +501,29 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{  
+  char path[MAXPATH], target[MAXPATH];
+  struct inode *ip;
+  if(argstr(0, target, MAXPATH) < 0)
+    return -1;
+  if(argstr(1, path, MAXPATH) < 0)
+    return -1;
+  begin_op();
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) < MAXPATH) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
